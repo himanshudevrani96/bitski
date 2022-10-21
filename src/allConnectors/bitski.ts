3964b117779c29a696da1bb819ea3450d34f2827
Himanshu Devrani <himanshudevrani@192.168.1.3>
2022-10-21 17:32:43 +0530

    route-vercel

--- src/allConnectors/bitski.ts
+++ src/allConnectors/bitski.ts
@@ -2,7 +2,7 @@ import { initializeConnector } from '@web3-react/core'
 import { BitskiConnect } from '../packages/bitski'
 
 const CLIENTID = 'a1d57ae2-3404-49dc-a848-edc39463c4fc'
-const callBackUrl = 'https://635280cb0f2692367a828e31--venerable-donut-de0de9.netlify.app/callback'
+const callBackUrl = 'https://bitski-nine.vercel.app/callback'
 const network = {
     rpcUrl: 'https://matic-mumbai.chainstacklabs.com',
     chainId: 80001,
